9bc8ac68a5ac386e4857798f253a56ab54bf6480
Manas Joshi <manassjoshi@gmail.com>
2022-10-07 11:26:14 +0530

    Added api folder

---
+++ api/server.js
@@ -0,0 +1,9 @@
+var express = require("express")
+var app = express()
+
+app.get("/api", (req, res) => {
+    res.send("API working successfully!")
+})
+
+const PORT = 8000
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
