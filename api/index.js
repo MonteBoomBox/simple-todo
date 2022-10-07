4913580c9552ef8ea0f95c21537f703f9b9d6ed5
Manas Joshi <manassjoshi@gmail.com>
2022-10-07 12:02:28 +0530

    Fixed response naming issue

--- api/index.js
+++ api/index.js
@@ -2,12 +2,15 @@ const axios = require("axios").default
 var express = require("express")
 var app = express()
 
+app.use(express.json())
+
 app.get("/api/test", (req, res) => {
     
-    axios.get("https://jsonplaceholder.typicode.com/todos/1").then((res) => {
-        res.send(res.data)
+    axios.get("https://jsonplaceholder.typicode.com/todos/1").then((response) => {
+        res.json(response.data)
     }).catch((err) => {
-        res.status(500).send(err)
+        console.log(err)
+        res.status(500).json(err)
     })
     
     // res.send("API working successfully!")
